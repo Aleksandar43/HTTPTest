c4a1d841f4565d65cd1a835d2e82c23a8e88edc9
Aleksandar Plahcinski <a.plahcinski@gmail.com>
2024-04-25 17:23:34 +0200

    First version of activity

---
+++ app/src/main/java/com/sourcico/httptest/MainActivity.java
@@ -0,0 +1,40 @@
+package com.sourcico.httptest;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button sendButton = findViewById(R.id.button_send);
+        TextView ipText = findViewById(R.id.text_ip_address);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ipText.setTextColor(Color.parseColor("#008800"));
+                ipText.setText("Button clicked");
+            }
+        });
+    }
+
+
+}

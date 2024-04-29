141d4e5f525b9a0ad88663744be3a62415f2fa34
Aleksandar Plahcinski <a.plahcinski@gmail.com>
2024-04-29 02:00:12 +0200

    Fixed sending private IPv4 addresses
    - Also moved address checking to functions to make the code cleaner

--- app/jni/getaddr.cpp
+++ app/jni/getaddr.cpp
@@ -13,9 +13,23 @@ extern "C" {
 
 static const char* TAG = "HTTPTest";
 
+bool ipv6_is_global_unicast(struct sockaddr_in6* ip6){
+    unsigned char *addr = ip6->sin6_addr.s6_addr;
+    return (addr[0] == 0x20 && addr[1] == 0x02) //2002::
+            || (addr[0] == 0x20 && addr[1] == 0x01 && addr[2] == 0x00 && addr[3] == 0x00); //2001:0::
+}
+
+bool ipv4_is_private(struct sockaddr_in* ip4){
+    unsigned int addr = htonl(ip4->sin_addr.s_addr);
+    return (addr & 0xFF000000) == 0x0A000000        //Class A private
+           || (addr & 0xFFF00000) == 0xAC100000     //Class B private
+           || (addr & 0xFFFF0000) == 0xC0A80000;    //Class C private
+}
+
 char* get_address(){
     struct sockaddr_in6 *good_ipv6 = NULL;
     struct sockaddr_in *good_ipv4 = NULL;
+    struct sockaddr_in *private_ipv4 = NULL;
 
     struct ifaddrs *address_list;
     getifaddrs(&address_list);
@@ -27,33 +41,26 @@ char* get_address(){
         struct sockaddr* sock = ifap->ifa_addr;
         if (sock != NULL) {
             if (sock->sa_family == AF_INET) {
-                struct sockaddr_in *ip4 = (struct sockaddr_in *) sock;
-                __android_log_print(ANDROID_LOG_INFO, TAG,"name=%s, sock->sa_family=%d, address=%s", name,sock->sa_family, inet_ntoa(ip4->sin_addr));
+                struct sockaddr_in *ipv4 = (struct sockaddr_in *) sock;
+                __android_log_print(ANDROID_LOG_INFO, TAG,"name=%s, sock->sa_family=%d, address=%s", name,sock->sa_family, inet_ntoa(ipv4->sin_addr));
 
-                unsigned int addr = htonl(ip4->sin_addr.s_addr);
-                if(!good_ipv4 && addr != INADDR_LOOPBACK
-                    && !(
-                            (addr & 0xFF000000) == 0x0A000000           //Class A private
-                            || (addr & 0xFFF00000) == 0xAC100000        //Class B private
-                            || (addr & 0xFFFF0000) == 0xC0A80000        //Class C private
-                    )
-                ){
-                    good_ipv4 = ip4;
+                unsigned int addr = htonl(ipv4->sin_addr.s_addr);
+                bool is_private = ipv4_is_private(ipv4);
+                if(!private_ipv4 && is_private){
+                    private_ipv4 = ipv4;
+                } else if(!good_ipv4 && !is_private && addr != INADDR_LOOPBACK){
+                    good_ipv4 = ipv4;
                 }
             } else if (sock->sa_family == AF_INET6) {
-                struct sockaddr_in6 *ip6 = (struct sockaddr_in6 *) sock;
+                struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *) sock;
                 char *string_addr = (char *) malloc(INET6_ADDRSTRLEN * sizeof(char));
-                inet_ntop(AF_INET6, &ip6->sin6_addr, string_addr, INET6_ADDRSTRLEN);
+                inet_ntop(AF_INET6, &ipv6->sin6_addr, string_addr, INET6_ADDRSTRLEN);
                 __android_log_print(ANDROID_LOG_INFO, TAG,"name=%s, sock->sa_family=%d, address=%s", name,sock->sa_family, string_addr);
                 free(string_addr);
 
-                //If an IPv6 address is available, that is from the global unicast range, also any address that has 0x0 for the first word is not a global unicast address.
-                unsigned char *addr = ip6->sin6_addr.s6_addr;
-                if(!good_ipv6 &&
-                    ((addr[0] == 0x20 && addr[1] == 0x02) //2002::
-                    || (addr[0] == 0x20 && addr[1] == 0x01 && addr[2] == 0x00 && addr[3] == 0x00) //2001:0::
-                )){
-                    good_ipv6 = ip6;
+                unsigned char *addr = ipv6->sin6_addr.s6_addr;
+                if(!good_ipv6 && ipv6_is_global_unicast(ipv6)){
+                    good_ipv6 = ipv6;
                     break; // No need to iterate further
                 }
             } else {
@@ -73,6 +80,9 @@ char* get_address(){
     } else if(good_ipv4){
         ret = (char*) malloc(INET_ADDRSTRLEN * sizeof(char));
         inet_ntop(AF_INET, &good_ipv4->sin_addr, ret, INET_ADDRSTRLEN);
+    } else if(private_ipv4){
+        ret = (char*) malloc(INET_ADDRSTRLEN * sizeof(char));
+        inet_ntop(AF_INET, &private_ipv4->sin_addr, ret, INET_ADDRSTRLEN);
     } else{
         ret = NULL;
     }
